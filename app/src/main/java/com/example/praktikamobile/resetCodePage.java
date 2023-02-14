44fd9df10b08041bc5185747e85c4d26a5ce66a3
HvRs <hvrsttk@gmail.com>
2023-02-14 22:08:49 +0700

    Добавлены окна восстановления пароля

---
+++ app/src/main/java/com/example/praktikamobile/resetCodePage.java
@@ -0,0 +1,14 @@
+package com.example.praktikamobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class resetCodePage extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reset_code_page);
+    }
+}
---
+++ app/src/main/java/com/example/praktikamobile/resetPasswordPage.java
@@ -0,0 +1,13 @@
+package com.example.praktikamobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+public class resetPasswordPage extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reset_password_page);
+    }
+}
