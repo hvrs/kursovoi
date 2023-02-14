095d40078d6b3b971ab088991b8c1dee84037887
HvRs <hvrsttk@gmail.com>
2023-02-14 21:31:04 +0700

    сделаны окна авторизации и регистрации

---
+++ app/src/main/java/com/example/praktikamobile/authorizationPage.java
@@ -0,0 +1,13 @@
+package com.example.praktikamobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+public class authorizationPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.authorization_page);
+    }
+}
