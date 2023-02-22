f6872998bbf5d984d454aea9cde99ea7cb60ae35
HvRs <hvrsttk@gmail.com>
2023-02-22 19:51:32 +0700

    Добавление окна стартовых настроек профиля пользователя

---
+++ app/src/main/java/com/example/praktikamobile/resetPassword_newp.java
@@ -0,0 +1,13 @@
+package com.example.praktikamobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+public class resetPassword_newp extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reset_password_newp);
+    }
+}
---
+++ app/src/main/java/com/example/praktikamobile/startSettingsPage.java
@@ -0,0 +1,14 @@
+package com.example.praktikamobile;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+public class startSettingsPage extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_settings_page);
+    }
+}
