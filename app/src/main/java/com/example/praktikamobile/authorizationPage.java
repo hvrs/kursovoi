7dd0ba74b58686d01577194fb350f6a128586937
HvRs <hvrsttk@gmail.com>
2023-03-05 03:48:59 +0700

    Добавление окна

--- app/src/main/java/com/example/praktikamobile/authorizationPage.java
+++ app/src/main/java/com/example/praktikamobile/authorizationPage.java
@@ -2,12 +2,21 @@ package com.example.praktikamobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 public class authorizationPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.authorization_page);
+    }
+    public void toNewActivity(View v)
+    {
+        Intent i = new Intent(getApplicationContext(),settingPage.class);
+        startActivity(i);
+
     }
 }
---
+++ app/src/main/java/com/example/praktikamobile/settingPage.java
@@ -0,0 +1,14 @@
+package com.example.praktikamobile;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+public class settingPage extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting_page);
+    }
+}
