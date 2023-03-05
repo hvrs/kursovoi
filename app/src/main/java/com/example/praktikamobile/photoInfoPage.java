01fae38d7238896167191fd5817a3ca0b2ef24a9
HvRs <hvrsttk@gmail.com>
2023-03-06 03:36:12 +0700

    Добавление окна, мелкие исправления

--- app/src/main/java/com/example/praktikamobile/photoInfoPage.java
+++ app/src/main/java/com/example/praktikamobile/photoInfoPage.java
@@ -5,18 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
-public class registrationPage extends AppCompatActivity {
+public class photoInfoPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.registration_page);
-    }
-    public void toNewActivity(View v)
-    {
-        Intent i = new Intent(getApplicationContext(),photoInfoPage.class);
-        startActivity(i);
-
+        setContentView(R.layout.photo_info_page);
     }
 }
