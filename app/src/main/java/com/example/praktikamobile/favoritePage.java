5306400eeadb799443a370ccba8f8b12f169be60
hvrs <hvrsttk@gmail.com>
2023-03-31 19:19:46 +0700

    ЗАкончено долбавление основных окон, два окна недоработаны(не имеют table)

---
+++ app/src/main/java/com/example/praktikamobile/favoritePage.java
@@ -0,0 +1,14 @@
+package com.example.praktikamobile;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+public class favoritePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.favorite_image_page);
+    }
+}
---
+++ app/src/main/java/com/example/praktikamobile/mainPage.java
@@ -0,0 +1,15 @@
+package com.example.praktikamobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+public class mainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_page);
+    }
+}
